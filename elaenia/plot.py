29034741bbf534f5121739805be54682f12f2f08
Dan Davison <dandavison7@gmail.com>
2020-01-31 17:10:20 -0600

    Improve setting of STFT parameters

--- elaenia/plot.py
+++ elaenia/plot.py
@@ -4,15 +4,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-N_FFT = 2 ** 10
-WIN_LENGTH = N_FFT
-HOP_LENGTH = 2 ** 8
+def stft(y, n_fft):
+    return np.abs(
+        lr.stft(
+            y, n_fft=n_fft, hop_length=_get_hop_length(n_fft), win_length=_get_win_length(n_fft)
+        )
+    )
 
 
-def stft(y):
-    ss = np.abs(lr.stft(y, n_fft=N_FFT, hop_length=HOP_LENGTH, win_length=WIN_LENGTH))
-    ss = np.log(ss + 1)
-    return ss
+def _get_n_fft(D):
+    half_n_fft = D.shape[0] - 1
+    assert _is_power_of_2(half_n_fft)
+    return half_n_fft << 1
+
+
+def _get_hop_length(n_fft):
+    assert _is_power_of_2(n_fft)
+    return n_fft >> 2
+
+
+def _get_win_length(n_fft):
+    assert _is_power_of_2(n_fft)
+    return n_fft
+
+
+def _is_power_of_2(n):
+    return n and not (n & (n - 1))
 
 
 def dist(s1, s2):
@@ -20,34 +37,36 @@ def dist(s1, s2):
     return ((s1 - s2) ** 2).sum()
 
 
-def distance_matrix(ss):
+def distance_matrix(D):
     "Matrix of distances between spectra at different time windows"
-    n_freq_bins, n_time_bins = ss.shape
+    n_freq_bins, n_time_bins = D.shape
     d = np.zeros((n_time_bins, n_time_bins))
     for i in range(n_time_bins):
         for j in range(i + 1):
-            d[i, j] = d[j, i] = np.linalg.norm(ss[:, i] - ss[:, j])
+            d[i, j] = d[j, i] = np.linalg.norm(D[:, i] - D[:, j])
     return 1 - d / d.max()
 
 
-def plot_spectrogram(ss, y_axis="hz", x_axis="s", sr=None, **kwargs):
-    lr.display.specshow(ss, x_axis=x_axis, y_axis=y_axis, sr=sr, hop_length=HOP_LENGTH, **kwargs)
+def plot_spectrogram(D, sr, y_axis="hz", x_axis="s", **kwargs):
+    hop_length = _get_hop_length(_get_n_fft(D))
+    D = np.log(D + 1)
+    lr.display.specshow(D, x_axis=x_axis, y_axis=y_axis, sr=sr, hop_length=hop_length, **kwargs)
 
 
-def plot_distance_matrix(ss, ax=None):
-    d = distance_matrix(ss)
+def plot_distance_matrix(D, ax=None):
+    dist = distance_matrix(D)
     ax = ax or plt.gca()
-    ax.imshow(np.log(d + 1))
+    ax.imshow(np.log(dist + 1))
 
 
-def plot_spectrogram_and_distance_matrix(ss, sr=None):
+def plot_spectrogram_and_distance_matrix(D, sr):
     "Plot spectrogram and time window distance matrix"
     # TODO: align the subplots
 
     fig = plt.figure(figsize=(16, 16))
 
     ax = fig.add_subplot(5, 1, 1)
-    plot_spectrogram(ss, ax=ax, sr=sr)
+    plot_spectrogram(D, sr, ax=ax)
 
     ax = fig.add_subplot(5, 1, (2, 5))
-    plot_distance_matrix(ss, ax=ax)
+    plot_distance_matrix(D, ax=ax)
