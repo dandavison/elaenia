dd84ab82260e5628b8d64d336fbb0fe5dd1ab9df
Dan Davison <dandavison7@gmail.com>
2020-02-05 18:11:57 -0600

    Xiphorhynchus whole genus experiment
    Confusion matrix:
    [[ 4  1  1  2  0  1  0  0  0  0  0  1  1]
     [ 0  7  2  3  0  3  0  0  0  0  0  1  0]
     [ 0  0  3  4  2  0  0  1  0  0  0  1  3]
     [ 0  3  0 10  3  0  0  0  0  0  0  1  1]
     [ 0  0  0  4 11  0  0  1  0  0  0  2  0]
     [ 0  0  0  6  2 10  0  0  0  0  0  0  0]
     [ 2  0  2  3  0  2  0  1  0  1  0  0  0]
     [ 0  3  1  0  3  2  1  3  0  0  0  1  2]
     [ 3  0  0  1  0  0  0  0  0  0  0  0  0]
     [ 3  1  2  1  1  0  0  0  0  0  0  2  0]
     [ 0  2  1  1  0  1  0  0  0  1  0  0  0]
     [ 0  1  1  6  2  2  0  0  0  0  0  6  0]
     [ 1  0  1  3  3  0  1  0  0  0  0  1  0]]
    Accuracy: 0.3176470588235294

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -1,2 +1,5 @@
 # tabulate labels in training data
 for f in data/index/Xiphorhynchus_guttatus_vs_elegans/*; do echo $f; cut -d/ -f1 < $f | sort | uniq -c; done
+
+# extract species labels
+prettier ~/tmp/elaenia/xeno-canto/dataset/metadata/gen_Xiphorhynchus/page*.json | jq .recordings[].sp | sort | uniq
