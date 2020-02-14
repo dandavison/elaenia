56fd19688320bc601edce461db5c4979e1afddf8
Dan Davison <dandavison7@gmail.com>
2020-02-14 15:10:47 -0600

    Add VGGish PCA pipeline

--- elaenia/pipelines/vggish.py
+++ elaenia/pipelines/vggish.py
@@ -1,11 +1,17 @@
 from sylph.learners.svm import SVMLearner
 from sylph.pipeline import Compose
 from sylph.pipeline import TrainingPipeline
+from sylph.transforms.pca import PCA
 from sylph.transforms.vggish import Audio2Spectrogram
 from sylph.transforms.vggish import Spectrogram2VGGishEmbeddings
 
 
 vggish_svm_learner_pipeline = TrainingPipeline(
     transform=Compose([Audio2Spectrogram(), Spectrogram2VGGishEmbeddings()]),
+    learn=SVMLearner(),
+)
+
+vggish_pca_svm_learner_pipeline = TrainingPipeline(
+    transform=Compose([Audio2Spectrogram(), Spectrogram2VGGishEmbeddings(), PCA(whiten=True)]),
     learn=SVMLearner(),
 )
