3e793a0875e97383e9998d017f402a6e221dd31d
Dan Davison <dandavison7@gmail.com>
2020-02-26 16:25:47 -0600

    WIP birdnet embeddings metrics

--- elaenia/experiments/birdnet_embeddings_Xiphorhynchus.py
+++ elaenia/experiments/birdnet_embeddings_Xiphorhynchus.py
@@ -1,10 +1,28 @@
 import pickle
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+from more_itertools import run_length
+import numpy as np
 from sylph.xeno_quero.dataset import XenoQueroDataset
+from toolz import groupby
 
 from elaenia.pipelines.birdnet import make_birdnet_embeddings_training_pipeline
 from elaenia.utils.color import red
+
+
+def plot_probabilities(output, line_color="red"):
+    dataset = output["transformed_testing_dataset"]
+    dataset["prediction_probs"] = output["transformed_testing_dataset_prediction_probs"]
+    row_species, row_species_counts = zip(*run_length.encode(dataset.labels))
+    idx = [sorted(row_species).index(sp) for sp in row_species]
+    plt.imshow(dataset["prediction_probs"][:, idx], aspect="auto")
+    row_species_offsets = [0] + list(np.cumsum(row_species_counts))
+    for col in range(len(row_species)):
+        xlim = [col - 0.5, col + 0.5]
+        ylim = [row_species_offsets[col], row_species_offsets[col + 1]]
+        plt.hlines(ylim, xmin=xlim[0], xmax=xlim[1], colors=line_color)
+        plt.vlines(xlim, ymin=ylim[0], ymax=ylim[1], colors=line_color)
 
 
 if __name__ == "__main__":
